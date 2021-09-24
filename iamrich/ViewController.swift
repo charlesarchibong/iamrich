8a96835a610349a806faa9ac2d90f1d4ad565538
charlesarchibong <charlesarchibong10@gmail.com>
2021-09-24 05:21:05 +0100

    Initial Commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Charles Archibong on 24/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
